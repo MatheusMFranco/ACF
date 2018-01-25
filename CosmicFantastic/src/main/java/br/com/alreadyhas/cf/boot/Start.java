5d635ef0c730433ac00ac4f89e701502ee2ada83
Matheus Franco <francomathmagal@gmail.com>
2018-01-24 23:54:41 -0300

    Class name renamed: Configuration to Start

--- CosmicFantastic/src/main/java/br/com/alreadyhas/cf/boot/Start.java
+++ CosmicFantastic/src/main/java/br/com/alreadyhas/cf/boot/Start.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Configuration {
+public class Start {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Configuration.class, args);
+		SpringApplication.run(Start.class, args);
 	}
 
 }
